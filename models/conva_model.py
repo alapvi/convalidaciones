b09bef1139cbe1f767c4876b536df2e3dd6c07d8
Alberto Aparicio <alapvi@yahoo.com>
2021-01-07 23:52:03 +0100

    Restricción SQL Convalidaciones

--- models/conva_model.py
+++ models/conva_model.py
@@ -6,6 +6,7 @@ from datetime import datetime
 class conva_model(models.Model):
     _name='convalidaciones.conva_model'
     _description = 'Modelo de Convalidaciones'
+    _sql_constraints = [('convalidaciones_alum_mod_unique','UNIQUE (modulo_id,alumno_id)','Alumno ya tiene la convalidación!'),]
 
     fecha=fields.Date(string="Fecha",required=True,help="Fecha obligatoria para convalidar",default=datetime.today())
     modulo_id=fields.Many2one("convalidaciones.modulo_model","modulo")
